3062c62ecaa16601aa8f113ea1f76bc826c2dced
anindyo-choudhury-1995 <anindyochoudhury1995@gmail.com>
2020-06-15 18:04:29 +0530

    3 files modified

--- Createtag.js
+++ Createtag.js
@@ -1,25 +1,71 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./createtag.css";
 class Createtag extends Component {
-  state = {
-    region: ["APAC", "EMEA", "LAD", "ANTARCTICA", "NAS", "JAPAN"]
-  };
+  state = {};
   render() {
     return (
       <div className="CreatetagContainer">
         <div className="TopActionBar">
-          <div>Create</div>
+          <div className="Create">Create</div>
           <div>
-            |
-            <button>
-              Save and Continue
-            </button>
-            <button>
-              Save and Close
-            </button>
-            <button>
-              Cancel
-            </button>
+            | &nbsp;
+            <button>Save and Continue</button>
+            <Link to="/tags">
+              <button>Save and Close</button>
+            </Link>
+            <Link to="/tags">
+              <button>Cancel</button>
+            </Link>
+          </div>
+        </div>
+        <br/>
+        <div className="TwoDivs">
+          <div className="FirstDiv">
+            <div className="Items">
+              <div className="ItemsFirst">* LookUp Type</div>
+              <div>
+                <input />
+              </div>
+            </div>
+            <div className="Items">
+              <div className="ItemsFirst">* LookUp Code</div>
+              <div>
+                <input />
+              </div>
+            </div>
+            <div className="Items">
+              <div className="ItemsFirst">* LookUp Meaning</div>
+              <div>
+                <input />
+              </div>
+            </div>
+            <div className="Items">
+              <div className="ItemsFirst">Active</div>
+              <div>
+                <input type="checkbox" />
+              </div>
+            </div>
+          </div>
+          <div className="SecondDiv">
+            <div className="Items">
+              <div className="ItemsFirst">* Region</div>
+              <div>
+                <input />
+              </div>
+            </div>
+            <div className="Items">
+              <div className="ItemsFirst">* Owner</div>
+              <div>
+                <input />
+              </div>
+            </div>
+            <div className="Items">
+              <div className="ItemsFirst">* Tags</div>
+              <div>
+                <input />
+              </div>
+            </div>
           </div>
         </div>
       </div>
